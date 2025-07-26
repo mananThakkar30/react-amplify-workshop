871e964ec958a4d61949a913639f128e5c5a4986
Manan Thakkar <manan.thakkar@brightpeers.com>
2025-07-26 13:12:00 +0530

    code update

--- src/App.js
+++ src/App.js
@@ -21,8 +21,7 @@ function App() {
 
     const apiUrl = await getApiUrl("");
     await axios.get(apiUrl).then((response) => {
-      const message = reponse?.data?.message;
-      document.getElementById("message").innerHTML = message ? message : response?.data;
+	document.getElementById("message").innerHTML = message ?? String(response?.data);
     }).catch((error) => {
       console.error("Error fetching users:", error);
     });
