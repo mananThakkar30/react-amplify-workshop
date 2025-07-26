c3faa44c959be7ff1029364a4d2a92149b0cc961
Manan Thakkar <manan.thakkar@brightpeers.com>
2025-07-26 12:43:11 +0530

    udpated fetch value

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ function App() {
 
     const apiUrl = await getApiUrl("");
     await axios.get(apiUrl).then((response) => {
-      document.getElementById("message").innerHTML = response?.data?.message;
+      const message = reponse?.data?.message;
+      document.getElementById("message").innerHTML = message ? message : response?.data;
     }).catch((error) => {
       console.error("Error fetching users:", error);
     });
